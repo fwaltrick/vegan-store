85ef906f22bd22fb95651e9d401668948784f35c
Fabricio Waltrick <fabricio.waltrick@gmail.com>
2020-08-29 14:03:51 +0200

    Fixed baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://deployment-url.now.sh"
+    ? "https://vegan-beauty-shop.herokuapp.com/"
     : "http://localhost:3000"
 
 export default baseUrl
